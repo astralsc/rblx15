12d7d0e3a06545012ccef1fed4b04faa3e734c65
Astral <132731805+astralsc@users.noreply.github.com>
2026-05-23 19:24:39 +0800

    update index

--- index.php
+++ index.php
@@ -23,6 +23,18 @@ $username = $_SESSION["user"]["username"];
 //please ffs fix this
 
 if(!isset($_SESSION['user'])){	
+    /*echo('<div class="card">
+  <div class="card-body">
+  <div id="liveAlertPlaceholder"></div>
+  <center>
+    <h1>
+      <h1 style="font-size: 50px;">RBLX15 | awesomesauce revival</h1>
+    </h1>
+    <i>Ready to experience 2015? Login with your discord account to get started!</i><br>
+<br> <a href="/login_discord.php" class="btn btn-primary me-md-1">Login with Discord</a>    <center>
+');*/
+
+
     echo('<div class="card">
   <div class="card-body">
   <div id="liveAlertPlaceholder"></div>
@@ -32,6 +44,7 @@ if(!isset($_SESSION['user'])){
     </h1>
     <i>Ready to experience 2015? Login with your discord account to get started!</i><br>
 <br> <a href="/login/" class="btn btn-primary me-md-1">Login</a> <a href="/register/" class="btn btn-primary me-md-1">Sign Up</a>');
+
 }else{
 	echo('<div class="card card-body render-main-card">
                 <div class="d-flex align-items-center">
@@ -53,7 +66,7 @@ if(!isset($_SESSION['user'])){
                             <h5> Friends (1) </h5>
                             <a class="d-inline-block text-decoration-none text-light me-3" style="max-width:100px" href="#">
                         <div class="position-relative">
-                            <!--<img src="https://cdn.sitetest1.rb15.us.to/pending.png" width="100" height="100" class="bg-light d-inline-block align-text-top rounded-circle">-->
+                            <!--<img src="https://cdn.sitetest1.localhost/pending.png" width="100" height="100" class="bg-light d-inline-block align-text-top rounded-circle">-->
                             <img src="https://devforum-uploads.s3.dualstack.us-east-2.amazonaws.com/uploads/original/4X/d/1/2/d120b245f694af8bb4ebb41c467e7686a168553a.png" width="100" height="100" class="bg-light d-inline-block align-text-top rounded-circle">
                                                     </div>
                         <p class="mb-0 text-center">ROBLOX</p>
